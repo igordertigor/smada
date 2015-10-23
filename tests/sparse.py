ad9031043d4aded93f02b8126f2f5d19b1c74cd0
Ingo Fründ <mail@ingofruend.net>
2015-10-23 19:33:09 +0200

    Some basic tests

---
+++ tests/sparse.py
@@ -0,0 +1,49 @@
+# import pytest
+import numpy as np
+
+from smada.sparse import LinARD
+
+
+def generate_fake_data(nobs, nfeatures, sparseness, noisesd=1., intercept=True):
+    Phi = np.random.randn(nobs, nfeatures)
+    if intercept:
+        Phi[:, 0] = 1.
+
+    weights = np.random.randn(nfeatures)
+    weights *= np.random.uniform(0, 1, nfeatures) < sparseness
+
+    y = np.dot(Phi, weights) + noisesd*np.random.randn(nobs)
+    return Phi, y, weights
+
+
+def test_sparse_solution():
+    Phi, y, w_gen = generate_fake_data(100, 20, .5)
+    model = LinARD(y, Phi)
+    model.train(100)
+
+    assert np.sum(abs(model.w) > 0) > .4
+    assert np.sum(abs(model.w) > 0) > .6
+
+
+def test_recover_sparseness():
+    Phi, y, w_gen = generate_fake_data(10000, 20, .5)
+    model = LinARD(y, Phi)
+    model.train(200)
+
+    # allow for up to 3 random mistake
+    print "This test is based on randomness. It may occasionally fail," \
+        " but you should get suspicious if it fails twice in a row."
+    assert np.sum((abs(model.w) < 1e-7) == (abs(w_gen) > 1e-7)) <= 3
+
+
+def test_more_data_better():
+    Phi, y, w_gen = generate_fake_data(20000, 20, .5)
+    model_small_data = LinARD(y[:1000], Phi[:1000])
+    model_big_data = LinARD(y[:15000], Phi[:15000])
+    model_small_data.train(100)
+    model_big_data.train(100)
+
+    y_small = model_small_data.predict(Phi[15000:])
+    y_big = model_big_data.predict(Phi[15000:])
+
+    assert np.sum((y[15000:]-y_small)**2) > np.sum((y[15000:]-y_big)**2)
