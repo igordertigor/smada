0133bae420102721fdea5876721a506b4c5fe2b3
Ingo Fründ <ingo.fruend@zalando.de>
2016-05-06 19:17:17 +0200

    removing now duplicate block_diag

--- smada/glm/expand.py
+++ smada/glm/expand.py
@@ -45,26 +45,6 @@ def bs_penalty(knots, degree):
     return C
 
 
-def block_diag(*matrices):
-    if matrices == ():
-        matrices = ([],)
-    matrices = [np.atleast_2d(m) for m in matrices]
-    bad_sizes = [k for k in range(len(matrices)) if matrices[k].ndim > 2]
-    if bad_sizes:
-        raise ValueError("Arguments in the following positions have dimensions"
-                         "greater than 2: {}".format(bad_sizes))
-
-    shapes = np.array([mat.shape for mat in matrices])
-    out = np.zeros(np.sum(shapes, axis=0), dtype=matrices[0].dtype)
-
-    r, c = 0, 0
-    for i, (rr, cc) in enumerate(shapes):
-        out[r:r+rr, c:c+cc] = matrices[i]
-        r += rr
-        c += cc
-    return out
-
-
 if __name__ == "__main__":
     knots = np.mgrid[0:1:10j]
     x = np.mgrid[0:1:100j]
