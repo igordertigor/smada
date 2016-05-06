7359dbb4ebaeb4ecfd1c8e028b5152f9c030d41b
Ingo Fründ <ingo.fruend@zalando.de>
2016-05-06 19:19:03 +0200

    illustrating optimized smoothness in gam example

--- examples/simple_gam.py
+++ examples/simple_gam.py
@@ -1,10 +1,13 @@
 """
 Simple example for a poisson GAM with a single smooth term
 """
+import operator
+import functools
+
 import numpy as np
 import pylab as pl
 
-from smada.glm import expand, glm
+from smada.glm import expand, glm, gcv, utils
 
 
 def f(x):
@@ -18,7 +21,8 @@ def example_simple_additive():
 
     knots = np.mgrid[0:5:20j]
     X = np.c_[np.ones(x.shape), x, expand.bs_expand(x, knots, 3)]
-    B = expand.block_diag(np.eye(2), expand.bs_penalty(knots, 3))
+    B_ = expand.bs_penalty(knots, 3)
+    B = utils.block_diag(np.eye(2), B_)
 
     # We don't yet have proper cross validation to estimate optimal smoothness.
     # Just estimate 3 different smoothness levels
@@ -29,14 +33,32 @@ def example_simple_additive():
     w2, r, converged = glm.estimate_glm(
         np.c_[X, y], glm.poisson_log_family, B)
 
+    th = np.exp(gcv.soptimize(
+        np.ones(2, 'd'),
+        functools.partial(gcv.gcv_as_error,
+                          penalties=[np.eye(2), B_],
+                          getR=functools.partial(gcv.getR_linear, R=np.c_[X, y]),
+                          nobservations=len(x))
+    ))
+
+    wbest, r, converged = glm.estimate_glm(
+        np.c_[X, y], glm.poisson_log_family,
+        penalty=utils.block_diag(*map(operator.mul, [np.eye(2), B_], th)))
+
     f0 = np.exp(np.dot(X, w0))
     f1 = np.exp(np.dot(X, w1))
     f2 = np.exp(np.dot(X, w2))
+    fbest = np.exp(np.dot(X, wbest))
 
+    pl.subplot(221)
     pl.plot(x, fx)
     pl.plot(x, y, 'o')
+    pl.plot(x, fbest)
+    pl.subplot(222)
     pl.plot(x, f0)
+    pl.subplot(223)
     pl.plot(x, f1)
+    pl.subplot(224)
     pl.plot(x, f2)
 
     pl.savefig('test.pdf')
