2e28d2bcd7c9707561feb4889fdd09092b85e01e
Ingo Fründ <mail@ingofruend.net>
2016-05-07 20:09:27 +0200

    fix r->r^2

--- smada/glm/gcv.py
+++ smada/glm/gcv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """
-Tools for setting regularization parameters based on generalized cross validation
+Tools for setting regularization parameters based on generalized cross
+validation
 """
 
 import operator
@@ -32,7 +33,7 @@ def gcv(R, Qy, B, n, r2=0.):
     d = len(D)
     dl = np.trace(np.dot(U[:d, :d], U[:d, :d].T))
     _, R_ = np.linalg.qr(np.r_[np.c_[R, Qy], np.c_[B, np.zeros(B.shape[0])]])
-    score = n*(r2+R_[-1, -1])/(n-dl)**2
+    score = n*(r2+R_[-1, -1]**2)/(n-dl)**2
     return score
 
 
@@ -64,7 +65,8 @@ def getR_linear(B, R):
 
     Args:
         B: cholesky factor of penalty matrix
-        R: R factor of the raw linear problem: np.c_[X,y] = QR. This also works for R=np.c_[X,y].
+        R: R factor of the raw linear problem: np.c_[X,y] = QR. This also works
+            for R=np.c_[X,y].
 
     Returns:
         R: R factor of the regularized problem with penalty $B^TB$
@@ -98,28 +100,34 @@ def soptimize(th0, error_func, niter=500, ftol=1e-7):
         f = f[ksort]
         th = th[ksort]
 
-        assert f[0] <= f[-1], 'Sorting failed: {}'.format(f)  # check that we have the right convention
+        # check that we have the right convention
+        assert f[0] <= f[-1], 'Sorting failed: {}'.format(f)
         centroid = th[:-1].mean(0)
 
-        reflected, fr = change_point_on_centroid(th[-1], centroid, error_func, al)
+        reflected, fr = change_point_on_centroid(
+            th[-1], centroid, error_func, al)
         if fr >= f[0] and fr < f[-2]:
             th[-1], f[-1] = reflected, fr
             continue
 
         if fr < f[0]:
-            expanded, fe = change_point_on_centroid(th[-1], centroid, error_func, gm)
+            expanded, fe = change_point_on_centroid(
+                th[-1], centroid, error_func, gm)
             th[-1], f[-1] = (expanded, fe) if fe < fr else (reflected, fr)
             continue
 
-        assert fr >= f[-2], '{} should be larger than {}'.format(fr, f[-2])  # just to make sure
-        contracted, fc = change_point_on_centroid(th[-1], centroid, error_func, rho)
+        # just to make sure
+        assert fr >= f[-2], '{} should be larger than {}'.format(fr, f[-2])
+        contracted, fc = change_point_on_centroid(
+            th[-1], centroid, error_func, rho)
 
         if fc < f[-1]:
             th[-1], f[-1] = contracted, fc
             continue
 
         res = map(
-            functools.partial(change_point_on_centroid, c=centroid, f=error_func, al=sg),
+            functools.partial(change_point_on_centroid,
+                              c=centroid, f=error_func, al=sg),
             th[1:])
         th[1:] = [v[0] for v in res]
         f[1:] = [v[1] for v in res]
